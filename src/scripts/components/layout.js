fc19d8ccf505d75ccb23f96166793b82b419c14b
gjjoo <joogeunjae@gmail.com>
2016-05-29 22:41:37 +0900

    layout add

---
+++ src/scripts/components/layout.js
@@ -0,0 +1,16 @@
+/**
+ * Created by gjjoo on 2016. 5. 24..
+ */
+
+export default class Layout {
+  static get CssClasses() {
+    return {};
+  }
+  constructor(element) {
+    this.element_ = element;
+  }
+  init() {
+    if (this.element_) {
+    }
+  }
+}
